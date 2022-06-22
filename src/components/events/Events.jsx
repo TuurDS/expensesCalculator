e173b06f5eb9f53c8304170bed1295d0bb69716f
Jitse Laceur Belon <jitse.laceurbelon@student.hogent.be>
2022-06-22 19:38:00 +0200

    some css met Kirby
    Co-authored-by: Kirby2020 <Kirby2020@users.noreply.github.com>

--- src/components/events/Events.jsx
+++ src/components/events/Events.jsx
@@ -4,7 +4,7 @@ import { useEvents } from '../../hooks/useEvents';
 import { useSession } from '../../hooks/useSession';
 
 import DefaultUserImg from "../../assets/img/default-user.png";
-import { faHeart } from '@fortawesome/free-solid-svg-icons'
+import { faThumbtack } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export default function Events() {
@@ -23,16 +23,23 @@ export default function Events() {
     {!loading && events.map((event, index) => {
         return (
             <div className='event' key={event.id}>
-                <div className='number'>{`${index + 1 < 10 ? "0": ""}${index+1}`}</div>
-                <div className='eventInfo'>
-                    <div className='eventTitle'>{event.name}</div>
-                    <div className='user'>
-                        <div className='userIcon'><img src={DefaultUserImg} alt="userImage" /></div> 
-                        <div className="text">{user.name}</div>
-                    </div>
+                <div className="event-left">
+
+                    <div className='number'>{`${index + 1 < 10 ? "0": ""}${index+1}`}</div>
+                    <div className='eventInfo'>
+                        <div className='eventTitle'>{event.name}</div>
+                        <div className='user'>
+                            <div className='userIcon'><img src={DefaultUserImg} alt="userImage" /></div> 
+                            <div className="text">{user.name}</div>
+                        </div>
+                    </div>  
                 </div>
-                <div className="pin"><FontAwesomeIcon icon={faHeart}/></div>
+                
+                <div className="event-right"> 
+                    <div className="pin"><FontAwesomeIcon icon={faThumbtack} className='icon'/></div>
+                </div> 
             </div>
+            
         )})
     }
     {loading && <div className="loading"><div className="loadingRing">loading</div></div>}
